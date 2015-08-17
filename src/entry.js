b44cf5357f6b2b43f245117f23167aad9c1a7713
Alexander <kemperomg@gmail.com>
2015-08-17 19:52:46 +0200

    Set up server with livereload - to run webpack & webpack-dev-server --content-base dist/

--- src/entry.js
+++ src/entry.js
@@ -5,7 +5,7 @@ require("./sass/styles.scss");
 require("./js/plugins.js");
 
 // TODO change this
-require("./js/vendor/modernizr.custom.92569.js");
+require("./js/vendor/modernizr.custom.92569");
 
 require("file?name=index.html!./index.html");
 /*
