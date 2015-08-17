4f87a7d1f3dd7d47e89ee7c5e503708155d2a9df
Alexander <kemperomg@gmail.com>
2015-08-17 19:52:46 +0200

    Set up server with livereload - to run webpack & webpack-dev-server --content-base dist/

--- dev-server.js
+++ dev-server.js
@@ -1,15 +1,64 @@
-/**
- * Created by Oleksandr Tserkovnyi on 05.08.2015.
- * kemperomg@gmail.com
- */
+var WebpackDevServer = require("webpack-dev-server");
+var webpack = require("webpack");
 
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config.js');
-var devServer = new WebpackDevServer(
-  webpack(config),
-  {
-    contentBase: './src',
-    publicPath: '/dist'
+var compiler = webpack({
+  entry: "./src/entry.js",
+  output: {
+    path: "./dist/",
+    filename: "js/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: "style!css!sass"
+      },
+      {
+        test: /\.css$/,
+        loader: "style!css"
+      },
+      {
+        test: /\.js$/,
+        loader: "babel",
+        exclude: "/node_modules/"
+      }
+    ]
   }
-).listen(8088, 'localhost');
+});
+var server = new WebpackDevServer(compiler, {
+  // webpack-dev-server options
+  contentBase: "dist/",
+  // or: contentBase: "http://localhost/",
+
+  hot: true,
+  // Enable special support for Hot Module Replacement
+  // Page is no longer updated, but a "webpackHotUpdate" message is send to the content
+  // Use "webpack/hot/dev-server" as additional module in your entry point
+  // Note: this does _not_ add the `HotModuleReplacementPlugin` like the CLI option does. 
+
+  // webpack-dev-middleware options
+  quiet: false,
+  noInfo: false,
+  lazy: true,
+  filename: "bundle.js",
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000
+  },
+  publicPath: "/assets/",
+  headers: { "X-Custom-Header": "yes" },
+  stats: { colors: true },
+
+  // Set this as true if you want to access dev server from arbitrary url.
+  // This is handy if you are using a html5 router.
+  historyApiFallback: false,
+
+  // Set this if you want webpack-dev-server to delegate a single path to an arbitrary server.
+  // Use "*" to proxy all paths to the specified server.
+  // This is useful if you want to get rid of 'http://localhost:8080/' in script[src],
+  // and has many other use cases (see https://github.com/webpack/webpack-dev-server/pull/127 ).
+  proxy: {
+    "*": "http://localhost:9090"
+  }
+});
+server.listen(8080, "localhost", function() {});
